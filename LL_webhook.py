604129f85079ee0c054a2a7eae718f775418784d
DrinkElixr <75812941+DrinkElixr@users.noreply.github.com>
2021-10-22 13:14:02 -0400

    Update LL_webhook.py

--- LL_webhook.py
+++ LL_webhook.py
@@ -293,5 +293,5 @@ def my_view_func(id,dd):
     return r.json()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(debug=True,threaded=True,host="0.0.0.0",port=8080)
+    #app.run(debug=True)
+    app.run(debug=True,threaded=True,host="0.0.0.0",port=8080)
